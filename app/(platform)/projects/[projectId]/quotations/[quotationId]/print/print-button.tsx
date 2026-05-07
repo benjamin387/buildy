0a295a9288f7a3f612aa4591c5c0fda7222dd48d
Benjamin Yeo <benjamin@urbanmotors.com.sg>
2026-05-07 17:53:35 +0800

    Upgrade quotation print package

---
+++ app/(platform)/projects/[projectId]/quotations/[quotationId]/print/print-button.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export function PrintButton() {
+  return (
+    <button
+      type="button"
+      onClick={() => window.print()}
+      className="inline-flex h-11 items-center justify-center rounded-xl bg-neutral-950 px-4 text-sm font-semibold text-white transition hover:bg-neutral-800"
+    >
+      Print (Save as PDF)
+    </button>
+  );
+}
